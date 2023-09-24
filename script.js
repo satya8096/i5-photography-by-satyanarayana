b6ed281204efa7259279816bee685f04cad3b74a
satya8096 <21MH5A0326@acoe.edu.in>
2023-09-24 13:06:57 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let Bars = document.querySelector(".nav-bars i");
+
+let Xmark = document.querySelector(".x-mark i");
+
+let NavBar = document.querySelector("header ul");
+
+
+Bars.onclick = () =>{
+    NavBar.style.width = "100%"
+}
+
+Xmark.onclick = () =>{
+    NavBar.style.width = "0%"
+}
+
+
+document.addEventListener("click",function(e){
+    if(e.target.classList.contains("gallery-img")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".img-show-container img").src = src;
+        document.querySelector(".img-show-container").style.scale = "1";
+    }
+})
+
+document.querySelector(".btn-close").onclick = () =>{
+    document.querySelector(".img-show-container").style.scale = "0"
+}
